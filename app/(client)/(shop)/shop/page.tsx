6e8983cc0728a51123119c2e20962df0b16785bc
sourabh7204 <sourabh722004@gmail.com>
2025-10-04 20:25:44 +0530

    Updated

--- app/(client)/(shop)/shop/page.tsx
+++ app/(client)/(shop)/shop/page.tsx
@@ -1,7 +1,7 @@
 import Shop from "@/components/Shop";
 import { getAllBrands, getCategories } from "@/sanity/queries";
 import React from "react";
-
+//Main Page
 const ShopPage = async () => {
   const categories = await getCategories();
   const brands = await getAllBrands();
