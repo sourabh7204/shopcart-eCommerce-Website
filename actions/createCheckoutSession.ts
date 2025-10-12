ce537009040a97fbc5ac566984258da3fbfaac12
sourabh7204 <sourabh722004@gmail.com>
2025-10-12 22:45:00 +0530

    Added Stripe Payment Method

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -6,6 +6,10 @@ import { urlFor } from "@/sanity/lib/image";
 import { CartItem } from "@/store";
 import Stripe from "stripe";
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2025-09-30.clover",
+});
+
 export interface Metadata {
   orderNumber: string;
   customerName: string;
